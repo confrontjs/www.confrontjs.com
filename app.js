091457c4b9d402d786c0143b1f3252987d3fdb67
Piotr Kowalski <piecioshka@gmail.com>
2019-08-14 09:55:26 +0200

    Create simple web server which redirect to $REDIRECT_URL

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const http = require('http');
+const process = require('process');
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: path.join(__dirname, 'app.env')
+})
+
+const url = process.env.REDIRECT_URL;
+const host = process.env.HOST;
+const port = Number(process.env.PORT);
+
+const server = http.createServer((req, res) => {
+    console.log(`${req.method} ${req.url}`);
+    res.writeHead(302, { 'Location': `${url}${req.url}` });
+    res.end();
+});
+
+server.listen(port, host, () => {
+    console.log(`Server was started at http://${host}:${port}`);
+});
